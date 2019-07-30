ff47ae2a5aa527c36d27c103050038efe10a7def
ericstarmars <lyic@163.com>
2019-07-30 14:59:54 +0800

    Replace old one

---
+++ common.js
@@ -0,0 +1,5 @@
+const datasrc_prefix = "https://raw.githubusercontent.com/ericstarmars/datavis_yangliu/master/";
+const nsclc_color = "steelblue";
+const smoke_color = "darkred"
+const life_color = "green";
+	
