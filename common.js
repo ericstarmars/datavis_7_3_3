a185cbd9740757ca46955293d4b12bca05c6ae3d
Yang Liu <lyic@163.com>
2019-07-30 23:49:33 +0800

    add chart6

--- common.js
+++ common.js
@@ -1,5 +1,7 @@
-const datasrc_prefix = "https://raw.githubusercontent.com/ericstarmars/datavis_yangliu/master/";
+//const datasrc_prefix = "https://raw.githubusercontent.com/ericstarmars/datavis_yangliu/master/";
+const datasrc_prefix = "";
 const nsclc_color = "steelblue";
-const smoke_color = "orange"
+const smoke_color = "darkorange"
 const life_color = "green";
+const peak_color = "red";
 	
